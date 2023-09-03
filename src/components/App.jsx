99c1a08f46a0d24a49a40480295709482062e677
Oleksandr Varga <vargasasha@gmail.com>
2023-09-03 13:11:36 +0300

    add spinner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,44 +3,41 @@ import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
 import { fetchImages } from './api/fetch';
-
-// export const App = () => {
-//   return (
-//     <>
-//       <Searchbar />
-//       <ImageGallery />
-//       <ImageItem/>
-//       <Button />
-//     </>
-//   );
-// };
+import { Spinner } from './Spinner/Spinner';
 
 export class App extends Component {
   state = {
     query: '',
     images: [],
     page: 1,
+    loading: false,
   };
 
   changeQuery = newQuery => {
     this.setState({
-      // query: `${Date.now()}/${newQuery}`,
-      query: newQuery,
+      query: `${Date.now()}/${newQuery}`,
       images: [],
       page: 1,
     });
   };
 
   async componentDidUpdate(prevProps, prevState) {
+    const query = this.state.query.slice(
+      this.state.query.indexOf('/') + 1,
+      this.state.query.length
+    );
+
     if (
       prevState.query !== this.state.query ||
       prevState.page !== this.state.page
     ) {
-      const images = await fetchImages(this.state.query, this.state.page);
+      this.setState({ loading: true });
 
-      // console.log(images.hits);
-
-      this.setState({ images: images.hits });
+      const images = await fetchImages(query, this.state.page);
+      this.setState(prevState => ({
+        images: [...prevState.images, ...images.hits],
+        loading: false,
+      }));
     }
   }
 
@@ -49,11 +46,15 @@ export class App extends Component {
   };
 
   render() {
+    const { images, loading } = this.state;
+
     return (
       <>
         <Searchbar changeQuery={this.changeQuery} />
-        <ImageGallery images={this.state.images} />
-        <Button loadMore={this.handleLoadMore} />
+
+        {loading ? <Spinner /> : <ImageGallery images={images} />}
+
+        {images.length > 0 && <Button loadMore={this.handleLoadMore} />}
       </>
     );
   }
